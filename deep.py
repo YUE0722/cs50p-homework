75e39598deb0a76edd1ea3df3449a164149c8697
YUE0722 <167620307+YUE0722@users.noreply.github.com>
2024-06-02 21:56:46 +0800

    Update deep.py

--- deep.py
+++ deep.py
@@ -1,8 +1,7 @@
 x = input("What is the Anwser to the Great Question of Life, the Universe and Everything? ")
+
 match x:
     case "42" | "forty-two" | "forty two":
         print("yes")
     case _ :
         print("No")
-
-
