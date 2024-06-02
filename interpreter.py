0ffda9b7b3791f9396d39d06cea6b671f71131fa
YUE0722 <167620307+YUE0722@users.noreply.github.com>
2024-06-02 21:55:21 +0800

    Add files via upload

---
+++ interpreter.py
@@ -0,0 +1,18 @@
+math = input("Expression: "). strip()
+x, y, z = math.split(" ")
+x = float(x)
+z = float(z)
+if y == "+":
+    print(f"{(x + z):.1f}")
+elif y == "-":
+    print(f"{(x - z):.1f}")
+elif y == "*":
+    print(f"{(x * z):.1f}")
+elif y == "/":
+    print(f"{(x / z):.1f}")
+
+
+
+
+
+
