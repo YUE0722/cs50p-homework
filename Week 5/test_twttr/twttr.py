e8e3df860eadac4b3ff4463589a986820b2b2a34
YUE0722 <167620307+YUE0722@users.noreply.github.com>
2024-06-09 22:14:07 +0800

    Create twttr.py

---
+++ Week 5/test_twttr/twttr.py
@@ -0,0 +1,15 @@
+def main():
+    word = input("Input: ")
+    print(f"Output: {shorten(word)}")
+
+def shorten(word):
+    output = []
+    for letter in word:
+        if letter in ["a", "e", "i", "o", "u"]:
+            continue
+        else:
+            output.append(letter)
+    return "".join(output)
+
+if __name__ == "__main__":
+    main()
