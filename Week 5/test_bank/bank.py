d9fd9e8a0ce8be390723e2122cd8aa097718cb05
YUE0722 <167620307+YUE0722@users.noreply.github.com>
2024-06-09 22:13:18 +0800

    Create bank.py

---
+++ Week 5/test_bank/bank.py
@@ -0,0 +1,15 @@
+def main():
+    greeting = input("Greeting: ")
+    print(f"${value(greeting)}")
+
+def value(greeting):
+    greeting = greeting.strip().lower()
+    if x.startswith("hello"):
+    return 0
+elif x.startswith("h"):
+    return 20
+else:
+    return 100
+
+if __name__ == "__main__":
+    main()
