ccb04470c746c9c688c6b279ebbe8bbcad38aa08
YUE0722 <167620307+YUE0722@users.noreply.github.com>
2024-06-09 22:42:51 +0800

    Create test_fuel.py

---
+++ Week 5/test_fuel/test_fuel.py
@@ -0,0 +1,20 @@
+from fuel import convert
+from fuel import gauge
+import pytest
+
+def test_convert():
+    assert test_convert("1/2") == 50
+    assert test_convert("1/3") == 33
+    with pytest.raises(ValueError):
+        convert("cat/dog")
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
+    with pytest.raises(ValueError):
+        convert("9/2")
+
+def test_gauge():
+    assert gauge(99) == "F"
+    assert gauge(100) == "F"
+    assert gauge(1) == "E"
+    assert gauge(0) == "E"
+    assert gauge(50) == "50%"
