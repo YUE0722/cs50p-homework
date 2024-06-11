75cd290abea1605ecca09ce0bb17c4b0ca93bc38
YUE0722 <167620307+YUE0722@users.noreply.github.com>
2024-06-11 21:26:01 +0800

    Update bank.py

--- Week 5/test_bank/bank.py
+++ Week 5/test_bank/bank.py
@@ -4,12 +4,12 @@ def main():
 
 def value(greeting):
     greeting = greeting.strip().lower()
-    if x.startswith("hello"):
-    return 0
-elif x.startswith("h"):
-    return 20
-else:
-    return 100
+    if greeting.startswith("hello"):
+        return 0
+    elif greeting.startswith("h"):
+        return 20
+    else:
+        return 100
 
 if __name__ == "__main__":
     main()
