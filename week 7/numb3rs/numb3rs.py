f6f9ef15aaa2b02526603720b069ce63456e7884
YUE0722 <167620307+YUE0722@users.noreply.github.com>
2024-06-13 18:08:29 +0800

    Update numb3rs.py

--- week 7/numb3rs/numb3rs.py
+++ week 7/numb3rs/numb3rs.py
@@ -4,11 +4,11 @@ def main():
     print(validate(input("IPv4 Address: ")))
 
 def validate(ip):
-    match = re.search(r"^(\d{1,3}\).(\d{1,3}\).(\d{1,3}\).(\d{1,3}\)$", ip)
+    match = re.search(r"^(\d{1,3}).(\d{1,3}).(\d{1,3}).(\d{1,3})$", ip)
     if not match:
         return False
     for group in match.groups():
-        if  int(group) < 1 or inte(group) > 255:
+        if  int(group) < 0 or int(group) > 255:
             return False
     return True
 
