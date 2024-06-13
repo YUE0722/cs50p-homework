cb78df6d47a8921615c7e4effe4be1f015b6fb3c
YUE0722 <167620307+YUE0722@users.noreply.github.com>
2024-06-13 18:04:13 +0800

    Update test_numb3rs.py

--- week 7/numb3rs/test_numb3rs.py
+++ week 7/numb3rs/test_numb3rs.py
@@ -1,8 +1,8 @@
 from numb3rs import validate
 
 def test_validate():
-    assert validate("0.0.0.0") == False
-    assert validate("256.256.256.256") == False
-    assert validate("hello") == False
-    assert validate("1.1.1.1") == True
+    assert validate("127.0.0.1") == True
     assert validate("255.255.255.255") == True
+    assert validate("512.512.512.512") == False
+    assert validate("1.2.3.1000") == False
+    assert validate("cat") == False
